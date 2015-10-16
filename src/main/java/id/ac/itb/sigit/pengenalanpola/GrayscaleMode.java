ba07e9978a00916000aea822cdc771249bcdd5bc
Hendy Irawan <ceefour666@gmail.com>
2015-10-16 17:53:10 +0700

    use invert, chaincode display in UI

--- src/main/java/id/ac/itb/sigit/pengenalanpola/GrayscaleMode.java
+++ src/main/java/id/ac/itb/sigit/pengenalanpola/GrayscaleMode.java
@@ -4,6 +4,12 @@ package id.ac.itb.sigit.pengenalanpola;
  * Created by Sigit on 16/10/2015.
  */
 public enum GrayscaleMode {
+    /**
+     * No inverse needed.
+     */
     WHITE_ON_BLACK,
+    /**
+     * Black on white, inverse needed.
+     */
     BLACK_ON_WHITE
 }
