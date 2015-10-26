b45e75e02265794702de27a38eeb6814db212c79
Hendy Irawan <ceefour666@gmail.com>
2015-10-26 13:13:02 +0700

    update

--- src/main/java/id/ac/itb/sigit/pengenalanpola/ZhangSuenFitur.java
+++ src/main/java/id/ac/itb/sigit/pengenalanpola/ZhangSuenFitur.java
@@ -7,15 +7,15 @@ import java.util.List;
  * Created by Sigit on 18/10/2015.
  */
 public class ZhangSuenFitur {
-    private int bulatan=0;
-    private List<ZhangSuenUjung> ujung=new ArrayList<>();
-    private  List<ZhangSuenSimpangan> simpangan=new ArrayList<>();
+    private int bulatan = 0;
+    private List<ZhangSuenUjung> ujung = new ArrayList<>();
+    private List<ZhangSuenSimpangan> simpangan = new ArrayList<>();
 
     public int getBulatan() {
         return bulatan;
     }
-    public String getBulatanString()
-    {
+
+    public String getBulatanString() {
         return String.valueOf(bulatan);
     }
 
@@ -26,17 +26,15 @@ public class ZhangSuenFitur {
     public List<ZhangSuenUjung> getUjung() {
         return ujung;
     }
-    public  String getUjungString()
-    {
-        if(ujung==null)
-        {
+
+    public String getUjungString() {
+        if (ujung == null) {
             return "";
         }
-        String msg=String.valueOf(ujung.size())+"==> ";
-        for(int i=0;i<ujung.size();i++)
-        {
-            msg=msg+"; point x : "+ String.valueOf(ujung.get(i).getEdge().getX())
-                    + " ,point y : "+String.valueOf(ujung.get(i).getEdge().getY());
+        String msg = String.valueOf(ujung.size()) + "==> ";
+        for (int i = 0; i < ujung.size(); i++) {
+            msg = msg + "; point x : " + String.valueOf(ujung.get(i).getEdge().getX())
+                    + " ,point y : " + String.valueOf(ujung.get(i).getEdge().getY());
         }
 
         return msg;
@@ -46,17 +44,14 @@ public class ZhangSuenFitur {
         return simpangan;
     }
 
-    public  String getSimpanganString()
-    {
-        if(simpangan==null)
-        {
+    public String getSimpanganString() {
+        if (simpangan == null) {
             return "";
         }
-        String msg=String.valueOf(simpangan.size())+"==> ";
-        for(int i=0;i<simpangan.size();i++)
-        {
-            msg=msg+"; point x : "+ String.valueOf(simpangan.get(i).getEdge().getX())
-                    + " ,point y : "+String.valueOf(simpangan.get(i).getEdge().getY());
+        String msg = String.valueOf(simpangan.size()) + "==> ";
+        for (int i = 0; i < simpangan.size(); i++) {
+            msg = msg + "; point x : " + String.valueOf(simpangan.get(i).getEdge().getX())
+                    + " ,point y : " + String.valueOf(simpangan.get(i).getEdge().getY());
         }
 
         return msg;
--- src/main/java/id/ac/itb/sigit/pengenalanpola/ZhangSuenService.java
+++ src/main/java/id/ac/itb/sigit/pengenalanpola/ZhangSuenService.java
@@ -23,71 +23,64 @@ public class ZhangSuenService {
     private boolean flag[][];
     private List<ZhangSuenFitur> zhangSuenFiturList;
 
-    public opencv_core.Mat loadInput(File imageFile,int mode)
-    {
+    public opencv_core.Mat loadInput(File imageFile, int mode) {
         log.info("Processing image file '{}' ...", imageFile);
         origMat = opencv_highgui.imread(imageFile.getPath());
-        grayOrigMat =opencv_highgui.imread(imageFile.getPath(), opencv_highgui.CV_LOAD_IMAGE_GRAYSCALE);
+        grayOrigMat = opencv_highgui.imread(imageFile.getPath(), opencv_highgui.CV_LOAD_IMAGE_GRAYSCALE);
         if (mode == 1) {
             final InverseImageConverter inverseImage = new InverseImageConverter(grayOrigMat);
             grayOrigMat = inverseImage.getInverseImage();
         }
-        zhangSuen=new ZhangSuen();
+        zhangSuen = new ZhangSuen();
         zhangSuenMat = zhangSuen.process(grayOrigMat);
         getListZhangSuenFitur();
         return origMat;
     }
 
-    public opencv_core.Mat loadInput(String contentType, byte[] inputBytes,int mode) {
+    public opencv_core.Mat loadInput(String contentType, byte[] inputBytes, int mode) {
         log.info("Processing input image {}: {} bytes ...", contentType, inputBytes.length);
         origMat = opencv_highgui.imdecode(new opencv_core.Mat(inputBytes), opencv_highgui.CV_LOAD_IMAGE_UNCHANGED);
-        grayOrigMat =opencv_highgui.imdecode(new opencv_core.Mat(inputBytes), opencv_highgui.CV_LOAD_IMAGE_GRAYSCALE);
+        grayOrigMat = opencv_highgui.imdecode(new opencv_core.Mat(inputBytes), opencv_highgui.CV_LOAD_IMAGE_GRAYSCALE);
         if (mode == 1) {
             final InverseImageConverter inverseImage = new InverseImageConverter(grayOrigMat);
             grayOrigMat = inverseImage.getInverseImage();
         }
-        zhangSuen=new ZhangSuen();
+        zhangSuen = new ZhangSuen();
         zhangSuenMat = zhangSuen.process(grayOrigMat);
         getListZhangSuenFitur();
         return origMat;
     }
 
-    public opencv_core.Mat getOrigMat()
-    {
+    public opencv_core.Mat getOrigMat() {
         return origMat;
     }
 
-    public opencv_core.Mat getZhainSuenMat()
-    {
+    public opencv_core.Mat getZhainSuenMat() {
         return zhangSuenMat;
     }
 
     //get objek pertama
-    public ZhangSuenFitur getZhangSuenFitur()
-    {
-        if(zhangSuenFiturList==null)
-        {
+    public ZhangSuenFitur getZhangSuenFitur() {
+        if (zhangSuenFiturList == null) {
             return new ZhangSuenFitur();
         }
         return zhangSuenFiturList.get(0);
     }
 
-    private List<ZhangSuenFitur> getListZhangSuenFitur()
-    {
-        flag=new boolean[zhangSuenMat.rows()][ zhangSuenMat.cols()];
-        zhangSuenFiturList=new ArrayList<>();
+    private List<ZhangSuenFitur> getListZhangSuenFitur() {
+        flag = new boolean[zhangSuenMat.rows()][zhangSuenMat.cols()];
+        zhangSuenFiturList = new ArrayList<>();
         final ByteIndexer imgIdx = zhangSuenMat.createIndexer();
 
-       // imgIdx.put(0,1,(byte)1);
+        // imgIdx.put(0,1,(byte)1);
 
 
         for (int y = 0; y < zhangSuenMat.rows(); y++) {
             for (int x = 0; x < zhangSuenMat.cols(); x++) {
-                int pxl = Byte.toUnsignedInt(imgIdx.get( y,x));
-                if(pxl>250 && ! flag[y][x])
-                {
-                    ZhangSuenFitur zhangSuenFitur=new ZhangSuenFitur();
-                    zhangSuenFitur= prosesZhangSuenFitur(x,y, imgIdx, zhangSuenFitur);
+                int pxl = Byte.toUnsignedInt(imgIdx.get(y, x));
+                if (pxl > 250 && !flag[y][x]) {
+                    ZhangSuenFitur zhangSuenFitur = new ZhangSuenFitur();
+                    zhangSuenFitur = prosesZhangSuenFitur(x, y, imgIdx, zhangSuenFitur);
                     zhangSuenFiturList.add(zhangSuenFitur);
                     log.info("zhangSuenFitur object #{} at ({}, {}): {}", zhangSuenFitur, x, y, zhangSuenFitur.getSimpangan());
                 }
@@ -97,77 +90,70 @@ public class ZhangSuenService {
         return zhangSuenFiturList;
     }
 
-    private ZhangSuenFitur prosesZhangSuenFitur(int x,int y,ByteIndexer idxImg,ZhangSuenFitur zhangSuenFitur)
-    {
-        if(flag[y][x])
-        {
+    private ZhangSuenFitur prosesZhangSuenFitur(int x, int y, ByteIndexer idxImg, ZhangSuenFitur zhangSuenFitur) {
+        if (flag[y][x]) {
             //buletan
-            zhangSuenFitur.setBulatan(zhangSuenFitur.getBulatan()+1);
+            zhangSuenFitur.setBulatan(zhangSuenFitur.getBulatan() + 1);
             return zhangSuenFitur;
         }
 
-       // flag[col][row]=true;
-        flag[y][x]=true;
+        // flag[col][row]=true;
+        flag[y][x] = true;
 
-        if((y - 1)<0 || (x - 1)<0 ||
-        (y+1) >= idxImg.rows()|| (x+1)>=idxImg.cols())
-        {
+        if ((y - 1) < 0 || (x - 1) < 0 ||
+                (y + 1) >= idxImg.rows() || (x + 1) >= idxImg.cols()) {
             return zhangSuenFitur;
         }
 
-        List<Edge> dataTetangga=new ArrayList<>();
+        List<Edge> dataTetangga = new ArrayList<>();
 
-        Edge edge1=new Edge(  x-1 ,y -1);
-        edge1.setvalue(idxImg.get( y-1,x-1));
+        Edge edge1 = new Edge(x - 1, y - 1);
+        edge1.setvalue(idxImg.get(y - 1, x - 1));
         dataTetangga.add(edge1);
 
-        Edge edge2=new Edge(  x ,y -1);
-        edge2.setvalue(idxImg.get( y-1,x));
+        Edge edge2 = new Edge(x, y - 1);
+        edge2.setvalue(idxImg.get(y - 1, x));
         dataTetangga.add(edge2);
 
-        Edge edge3=new Edge(  x+1 ,y -1);
-        edge3.setvalue(idxImg.get( y-1,x+1));
+        Edge edge3 = new Edge(x + 1, y - 1);
+        edge3.setvalue(idxImg.get(y - 1, x + 1));
         dataTetangga.add(edge3);
 
-        Edge edge4=new Edge(  x+1 ,y);
-        edge4.setvalue(idxImg.get( y,x+1));
+        Edge edge4 = new Edge(x + 1, y);
+        edge4.setvalue(idxImg.get(y, x + 1));
         dataTetangga.add(edge4);
 
-        Edge edge5=new Edge(  x+1 ,y +1);
-        edge5.setvalue(idxImg.get( y + 1,x+1 ));
+        Edge edge5 = new Edge(x + 1, y + 1);
+        edge5.setvalue(idxImg.get(y + 1, x + 1));
         dataTetangga.add(edge5);
 
-        Edge edge6=new Edge(  x ,y +1);
-        edge6.setvalue(idxImg.get( y + 1,x ));
+        Edge edge6 = new Edge(x, y + 1);
+        edge6.setvalue(idxImg.get(y + 1, x));
         dataTetangga.add(edge6);
 
-        Edge edge7=new Edge(  x-1 ,y +1);
-        edge7.setvalue(idxImg.get( y + 1,x-1 ));
+        Edge edge7 = new Edge(x - 1, y + 1);
+        edge7.setvalue(idxImg.get(y + 1, x - 1));
         dataTetangga.add(edge7);
 
-        Edge edge8=new Edge(  x-1 ,y );
-        edge8.setvalue(idxImg.get( y ,x-1 ));
+        Edge edge8 = new Edge(x - 1, y);
+        edge8.setvalue(idxImg.get(y, x - 1));
         dataTetangga.add(edge8);
 
-        List<Edge> nextStep=new ArrayList<>();
+        List<Edge> nextStep = new ArrayList<>();
 
-        int temppotition=-1;
+        int temppotition = -1;
 
-        boolean ujung=false;
-        boolean cabang=true;
+        boolean ujung = false;
+        boolean cabang = true;
 
-        for(int i=0;i<dataTetangga.size();i++)
-        {
-            if(dataTetangga.get(i).getvalue()!=0)
-            {
-                if(temppotition<0)
-                {
-                    ujung=true;
+        for (int i = 0; i < dataTetangga.size(); i++) {
+            if (dataTetangga.get(i).getvalue() != 0) {
+                if (temppotition < 0) {
+                    ujung = true;
                     nextStep.add(dataTetangga.get(i));
-                    temppotition=i;
-                }
-                else {
-                    ujung=false;
+                    temppotition = i;
+                } else {
+                    ujung = false;
                     nextStep.add(dataTetangga.get(i));
                     int cek = i - temppotition;
                     if (cek < 2) {
@@ -176,47 +162,37 @@ public class ZhangSuenService {
                     temppotition = i;
                 }
 
-                if(i==dataTetangga.size()-1 && dataTetangga.get(0).getvalue()!=0)
-                {
+                if (i == dataTetangga.size() - 1 && dataTetangga.get(0).getvalue() != 0) {
                     cabang = false;
                 }
             }
         }
-        if(nextStep.size()==0)
-        {
+        if (nextStep.size() == 0) {
             //cuma 1 pixel;
             return zhangSuenFitur;
-        }
-        else if(ujung && nextStep.size()<2)
-        {
+        } else if (ujung && nextStep.size() < 2) {
             //ujung
-            ZhangSuenUjung zhangSuenUjung=new ZhangSuenUjung();
+            ZhangSuenUjung zhangSuenUjung = new ZhangSuenUjung();
             zhangSuenUjung.setEdge(nextStep.get(0));
             zhangSuenFitur.getUjung().add(zhangSuenUjung);
-            zhangSuenFitur= prosesZhangSuenFitur(nextStep.get(0).getX(),nextStep.get(0).getY(), idxImg, zhangSuenFitur);
+            zhangSuenFitur = prosesZhangSuenFitur(nextStep.get(0).getX(), nextStep.get(0).getY(), idxImg, zhangSuenFitur);
             return zhangSuenFitur;
-        }
-        else if (cabang && nextStep.size()>2)
-        {
+        } else if (cabang && nextStep.size() > 2) {
             //cabang
 
             ZhangSuenSimpangan zhangSuenSimpangan = new ZhangSuenSimpangan();
-            zhangSuenSimpangan.setEdge(new Edge(y,x));
+            zhangSuenSimpangan.setEdge(new Edge(y, x));
             zhangSuenSimpangan.getPoints().addAll(nextStep);
             zhangSuenFitur.getSimpangan().add(zhangSuenSimpangan);
-            for(int i=0;i<nextStep.size();i++)
-            {
-                if(!flag[nextStep.get(i).getY()][nextStep.get(i).getX()]) {
+            for (int i = 0; i < nextStep.size(); i++) {
+                if (!flag[nextStep.get(i).getY()][nextStep.get(i).getX()]) {
                     zhangSuenFitur = prosesZhangSuenFitur(nextStep.get(i).getX(), nextStep.get(i).getY(), idxImg, zhangSuenFitur);
                 }
             }
-            return  zhangSuenFitur;
-        }
-        else
-        {
-            for(int i=0;i<nextStep.size();i++)
-            {
-                if(!flag[nextStep.get(i).getY()][nextStep.get(i).getX()]) {
+            return zhangSuenFitur;
+        } else {
+            for (int i = 0; i < nextStep.size(); i++) {
+                if (!flag[nextStep.get(i).getY()][nextStep.get(i).getX()]) {
                     zhangSuenFitur = prosesZhangSuenFitur(nextStep.get(i).getX(), nextStep.get(i).getY(), idxImg, zhangSuenFitur);
                 }
             }
