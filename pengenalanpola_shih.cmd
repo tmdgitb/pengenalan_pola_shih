88387ea9ffccf712e7e896c2b1047a72f278f75b
Hendy Irawan <ceefour666@gmail.com>
2015-12-20 20:01:44 +0700

    update cmd

--- pengenalanpola_shih.cmd
+++ pengenalanpola_shih.cmd
@@ -1,2 +1,2 @@
-java -jar target\pengenalanpola-0.0.1-SNAPSHOT.jar
-rem java -Djava.awt.headless=true -cp "target\classes;target\dependency\*.jar" id.ac.itb.sigit.pengenalanpola.DaemonApp %*
+rem java -jar target\pengenalanpola-0.0.1-SNAPSHOT.jar
+java -Djava.awt.headless=true -cp target/dependency/*;target/classes id.ac.itb.sigit.pengenalanpola.DaemonApp %*
